3d61b436b619d6390c406830cd5de5cd678b9710
Carlos Vendrell <carlos.vendrell@tigerspike.com>
2021-08-25 10:13:10 +0200

    testListImagesScrollableHorizontally() implemented to cover scenario 1 - second Jira ticket

--- app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
@@ -14,6 +14,7 @@ import org.junit.Rule
 import org.junit.Test
 
 private const val FIRST_POSITION = 0
+private const val SECOND_POSITION = 1
 private const val VALID_USER_NAME = "user1"
 private const val VALID_USER_PASSWORD = "password"
 private const val INVALID_USER_NAME = "uuuser1"
@@ -115,5 +116,34 @@ class LoginInstrumentedTest {
         softCloseUsingHomeButtonAndRelaunchTheApp()
 
         firstImage.awaitUntilDisplayed()
+    }
+
+    @Test
+    fun testListImagesScrollableHorizontally() {
+        val editTextUserName = R.id.editTextUserName
+        val editTextPassword = R.id.editTextPassword
+        val buttonLogin = R.id.buttonLogin
+        val recyclerView = R.id.recyclerViewNews
+        val image = R.id.imageView
+        val firstImage: Matcher<View> = allOf(
+            withId(image),
+            isDescendantOfA(
+                childAtPosition(withId(recyclerView), FIRST_POSITION)
+            )
+        )
+        val secondImage: Matcher<View> = allOf(
+            withId(image),
+            isDescendantOfA(
+                childAtPosition(withId(recyclerView), SECOND_POSITION)
+            )
+        )
+
+        typeTextOn(editTextUserName, VALID_USER_NAME)
+        typeTextOn(editTextPassword, VALID_USER_PASSWORD)
+        clickOn(buttonLogin)
+        firstImage.awaitUntilDisplayed()
+
+        swipeLeft(firstImage)
+        swipeLeft(secondImage)
     }
 }
--- app/src/androidTest/java/com/test/news/utils/Interactions.kt
+++ app/src/androidTest/java/com/test/news/utils/Interactions.kt
@@ -1,9 +1,11 @@
 package com.test.news.utils
 
+import android.view.View
 import androidx.annotation.IdRes
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matcher
 
 fun clickOn(@IdRes id: Int) {
     onView(withId(id)).perform(click())
@@ -12,4 +14,8 @@ fun clickOn(@IdRes id: Int) {
 fun typeTextOn(@IdRes id: Int, text: String) {
     onView(withId(id)).perform(clearText())
     onView(withId(id)).perform(typeText(text))
+}
+
+fun swipeLeft(viewMatcher: Matcher<View>) {
+    onView(viewMatcher).perform(swipeLeft())
 }
